5c1fb43028e7a17c4f1ce2e43a059f05ea938865
Ismail mayito <ismayito1@gmail.com>
2026-06-09 16:13:11 +0400

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by mac on 09/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
