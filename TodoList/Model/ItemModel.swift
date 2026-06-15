55cb9ea02e56dbd8063e5d61e409c12b454d2950
Ismail mayito <ismayito1@gmail.com>
2026-06-15 21:52:09 +0400

    update: Update model structure

---
+++ TodoList/Model/ItemModel.swift
@@ -0,0 +1,26 @@
+//
+//  ItemModel.swift
+//  TodoList
+//
+//  Created by mac on 10/06/2026.
+//
+
+import Foundation
+
+//Immutable struct
+
+struct ItemModel: Identifiable {
+    let id: String
+    let title: String
+    let isCompleted: Bool
+
+    init(id: String = UUID().uuidString, title: String, isCompleted: Bool) {
+        self.id = id
+        self.title = title
+        self.isCompleted = isCompleted
+    }
+    
+    func updateCompletion()->ItemModel{
+        return ItemModel(id: id, title: title, isCompleted: !isCompleted)
+    }
+}
